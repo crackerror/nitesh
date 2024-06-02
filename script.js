4612b50899b80c71aab03e4a494ffcefad7b68e3
crackerror <crackfighter789@gmail.com>
2024-06-02 18:00:45 +0530

    Add folder

---
+++ script.js
@@ -0,0 +1,52 @@
+gsap.from("#nav #right h3",{
+y:-200,
+duration:0.8,
+stagger:0.5
+})
+
+gsap.from("#nav #left h1",{
+    x:-200,
+    duration:1,
+
+})
+
+gsap.from("#page1 #left",{
+    x:-200,
+    duration:2
+})
+
+gsap.from("#page2 #left",{
+    x:-200,
+    duration:1
+})
+
+
+gsap.from("#page3 #box",{
+    x:500,
+    duration:1,
+    stagger:0.5
+})
+
+gsap.from("#why #left h1",{
+    x:-200,
+    duration:1,
+    stagger:0.5
+})
+
+gsap.from("#page4 #box4",{
+    y:200,
+    duration:1,
+    stagger:1
+})
+
+gsap.from("#page5 #img5",{
+    y:-200,
+    duration:1,
+    stagger:0.5
+})
+
+gsap.from("#round",{
+    x:-1000,
+    duration:2,
+
+})
